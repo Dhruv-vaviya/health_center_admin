6896796d31c993beb5be27df02846289a9f07db9
Dhruv-vaviya <dhruvvaviyablog@gmail.com>
2026-05-08 10:29:54 +0530

    upgrade login and add toastify

--- src/context/AdminContext.jsx
+++ src/context/AdminContext.jsx
@@ -1,12 +1,18 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 
 export const AdminContext = createContext()
 
 const AdminContextProvider = (props) => {
 
-    const value = {
+    const [atoken, setAToken] = useState('')
 
+    const backendUrl = import.meta.env.VITE_BACKEND_URL
+
+    const value = {
+        atoken,
+        setAToken,
+        backendUrl
     }
 
     return(
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,21 +1,56 @@
-import { useState } from 'react'
-import {assets} from '../assets/assets'
+import { useContext, useState } from 'react'
+import {AdminContext} from '../context/AdminContext'
+import axios from 'axios'
+import { toast } from 'react-toastify'
+import { data } from 'react-router-dom'
 
 const Login = () => {
 
     const [state, setState] = useState('Admin')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const {setAToken, backendUrl} = useContext(AdminContext)
+
+    const onSubmitHandler = async (event) => {
+
+        event.preventDefault()
+
+        try {
+            
+            if(state === 'Admin'){
+                const {data} = await axios.post(backendUrl + '/api/admin/login', {email,password})
+                if(data.success){
+                    localStorage.setItem('aToken', data.token)
+                    setAToken(data.token)
+                }
+            }else{
+                toast.error(data.message);
+            }
+
+        } catch (error) {
+            toast.error(error.message);
+        }
+
+    }
 
 return (
-    <form className='min-h-[80vh] flex items-center'>
+    <form onSubmit={onSubmitHandler} className='min-h-[80vh] flex items-center'>
         <div className='flex flex-col gap-3 m-auto items-start p-8 min-w-85 sm:min-w-96 border rounded-xl text-[#5E5E5E] text-sm shadow-xl'>
             <p className='text-2xl font-semibold m-auto'><span className='text-[#5F6FFF]'> {state} </span> Login</p>
             <div className='w-full'>
                 <p>Email</p>
-                <input className='border border-[#DADADA] rounded w-full p-2 mt-1' type="email" required />
+                <input
+                onChange={(e)=> setEmail(e.target.value)}
+                value={email}
+                className='border border-[#DADADA] rounded w-full p-2 mt-1' type="email" required />
             </div>
             <div className='w-full'>
                 <p>Password</p>
-                <input className='border border-[#DADADA] rounded w-full p-2 mt-1' type="password" required />
+                <input
+                onChange={(e)=> setPassword(e.target.value)}
+                value={password}
+                className='border border-[#DADADA] rounded w-full p-2 mt-1' type="password" required />
             </div>
             <button className='bg-[#5F6FFF] text-white w-full py-2 rounded-md text-base hover:bg-[#3f4eb1]'>Login</button>
             {
